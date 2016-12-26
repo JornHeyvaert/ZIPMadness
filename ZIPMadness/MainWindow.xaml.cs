b22a17651308ab72850f220e46834cde278ea08b
Jorn Heyvaert <jorn.heyvaert@telenet.be>
2016-12-26 15:38:42 +0100

    Initial commit

--- ZIPMadness/MainWindow.xaml.cs
+++ ZIPMadness/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.IO;
 
 namespace ZIPMadness
 {
@@ -23,6 +13,100 @@ namespace ZIPMadness
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private bool isFolderUsed = false;
+
+        private void btnFiles_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == true)
+            {
+                string[] selectedFiles = ofd.FileNames;
+                txtItemsToCompress.Text = string.Join(",", selectedFiles);
+                isFolderUsed = false;
+            }
+        }
+
+        private void btnFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                txtItemsToCompress.Text = fbd.SelectedPath;
+                isFolderUsed = true;
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog();
+            sfd.Filter = "ZIP files (.zip)|*.zip";
+
+            if (!string.IsNullOrEmpty(txtItemsToCompress.Text))
+            {
+                if (sfd.ShowDialog() == true)
+                {
+                    if (isFolderUsed)
+                    {
+                        ZipFile.CreateFromDirectory(txtItemsToCompress.Text, sfd.FileName);
+                    }
+                    else
+                    {
+                        string[] files = txtItemsToCompress.Text.Split(',');
+                        using (ZipArchive zip = ZipFile.Open(sfd.FileName, ZipArchiveMode.Create))
+                        {
+                            foreach (string file in files)
+                            {
+                                zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                            }
+                        }
+                    }
+                    System.Windows.MessageBox.Show("ZIP file created succesfully!!!");
+                }
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("File(s) or Folder must be selected to create ZIP file!!!");
+            }
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                using (ZipArchive zip = ZipFile.OpenRead(ofd.FileName))
+                {
+                    foreach (ZipArchiveEntry entry in zip.Entries)
+                    {
+                        lbContains.Items.Add(entry.FullName);
+                    }
+                }
+            }
+        }
+
+        private void btnExtract_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "ZIP files (.zip)|*.zip";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                txtExtractLocation.Text = ofd.FileName;
+                DialogResult result = fbd.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    ZipFile.ExtractToDirectory(ofd.FileName, fbd.SelectedPath);
+                    System.Windows.MessageBox.Show("ZIP file extracted succesfully!");
+                }
+            }
         }
     }
 }
